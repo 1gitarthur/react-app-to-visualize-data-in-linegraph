1955d228dee4dc5b7e61ea8340762aebf619952f
Arthur Billy <122879361+1gitarthur@users.noreply.github.com>
2023-01-26 22:11:55 -0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+import LineGraph from "./LineGraph"
+const data = [
+  { label: 'January', value: 100 },
+  { label: 'February', value: 200 },
+  { label: 'March', value: 300 },
+  // ...
+];
+
+<LineGraph data={data} />
